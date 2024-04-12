3e2a99f5e716085ef1614cb0a2b2a5a7dbe94a9d
serwis <pocitace.lbc@gmail.com>
2024-04-12 13:36:07 +0200

    Uzivatel v Tutulku

---
+++ js/jmeno.js
@@ -0,0 +1,10 @@
+//Napište program, který se zeptá uživatele na jméno a poté jeho jméno vypište do titulku stránky.
+
+console.log(document);
+
+
+
+let uzivatel = prompt("Vase jmeno");
+console.log(uzivatel);
+
+document.title = uzivatel;
